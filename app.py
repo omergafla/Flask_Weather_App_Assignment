b7796d83016a03b65bcea90bfaab25b86d0cf290
omergafla <omergafla@gmail.com>
2020-12-29 12:15:09 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, render_template
+import requests
+
+
+app = Flask(__name__)
+
+@app.route('/get_lowest_temp')
+def get_weather():
+    API_KEY = '4f6ba86016ff1332409b755f3839bfb6'
+    cities = ["Tel-Aviv", "Berlin", "Budapest"]
+    result = {}
+    for city in cities:
+        url = "http://api.openweathermap.org/data/2.5/forecast?q={}&&units=metric&appid={}".format(city, API_KEY)
+        r = requests.get(url)
+        r_json = r.json()
+        min_temperature = r_json["list"][0]["main"]["temp_min"]
+        min_temperature_main = r_json["list"][0]["main"]
+        for entry in r_json["list"]:
+            if entry["main"]["temp_min"] <= min_temperature:
+                min_temperature = entry["main"]["temp_min"]
+                min_temperature_main = entry["main"]
+
+        result[city] = min_temperature_main
+    print(result)
+
+    coldest_temperature = result["Tel-Aviv"]["temp_min"]
+    coldest_temperature_main = result["Tel-Aviv"]
+    coldest_temperature_main["city"] = "Tel-Aviv"
+    for city in result:
+        if result[city]["temp_min"] < coldest_temperature:
+            coldest_temperature = result[city]["temp_min"]
+            coldest_temperature_main = result[city]
+            coldest_temperature_main["city"] = city
+    return coldest_temperature_main
+
+
+@app.route('/')
+def index():
+    coldest_city = get_weather()
+    # city = coldest_city["city"]
+    # temp = coldest_city["temp"]
+    # feels_like = coldest_city["feels_like"]
+    # temp_min = coldest_city["temp_min"]
+    # temp_max = coldest_city["temp_max"]
+    # pressure = coldest_city["pressure"]
+    # sea_level = coldest_city["sea_level"]
+    # grnd_level = coldest_city["grnd_level"]
+    # humidity = coldest_city["humidity"]
+
+    return render_template("index.html", coldest_city=coldest_city)
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
